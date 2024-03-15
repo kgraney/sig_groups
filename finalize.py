e1b646ac09baec5d231e98031c691f045c7c4e50
allison-fisher-404 <56833354+allison-fisher-404@users.noreply.github.com>
2024-03-14 23:12:44 -0400

    Update finalize.py
    Fix runtime error due to extra parameter, and add explicit guidance about required code change.

--- finalize.py
+++ finalize.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.insert(1, '/usr/local/google/home/kmg/')
 sys.path.insert(1, '/mnt/c/Users/Kevin Graney/SIG Groupings/')
+sys.path.insert(1, '/usr/local/google/home/amfisher/dsig24')
+sys.path.insert(1, '/mnt/c/Users/Allison Fisher/SIG Groupings/')
 
 from sig_groups.ride import Ride, Rosters
 from sig_groups.config import LoadConfigFile
@@ -13,13 +15,16 @@ config = LoadConfigFile("configs/2024.yaml")
 rides = [Ride(x) for x in config.Rides()]
 
 airtable_client = AirtableClient(config.Airtable(), config.Rides())
-rider_data = RiderData(airtable_client.LoadLeaders(), airtable_client.LoadParticipants(), [])
+rider_data = RiderData(airtable_client.LoadLeaders(), airtable_client.LoadParticipants())
 
 rosters = airtable_client.GetPriorRosters(rider_data)
 
 slack_client = SlackClient(config.Slack(), rides)
 PrintRosters(rosters, rider_data)
-for ride in [8]:
+
+# TODO: update for the ride you want to finalize.
+finalize_through_ride_n = 0
+for ride in [finalize_through_ride_n]:
     ride_rosters = Rosters(r for r in rosters if r.ride == ride)
     slack_client.PostRoster(ride_rosters)
 
